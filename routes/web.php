d68680f03d770aeed646f7065291a53375268125
aykuanysh <a.kuanyshev11@gmail.com>
2026-01-25 15:28:50 +0500

    fix: add import for NoteController in routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NoteController;
 
+// перенаправляем с главной страницы на список заметок
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->router('notes.index');
 });
+
+// автоматически создает 7 маршрутов CRUD и связывает с методами NoteContoller
+Route::resource('notes', NoteController::class);
